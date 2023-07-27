0617fbb2ca44718fef831d7b4f5d064ad3da2374
Demkiv, Lyubomir <Lyubomir.Demkiv@adesso.de>
2023-07-27 15:00:59 +0200

    initialized project

---
+++ src/main/java/de/adesso/projects/sapshowcase/SpringSapShowcaseApplication.java
@@ -0,0 +1,13 @@
+package de.adesso.projects.sapshowcase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSapShowcaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSapShowcaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/adesso/projects/sapshowcase/SpringSapShowcaseApplicationTests.java
@@ -0,0 +1,13 @@
+package de.adesso.projects.sapshowcase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSapShowcaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
